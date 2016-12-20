e6cc855a8e377235f25da63a4289b525862b523c
Travis Collins <travisfcollins@gmail.com>
2016-12-20 14:23:47 -0500

    Added PV pair arguments and checks for input files and MATLAB versions

--- MakeFigureLatexReady.m
+++ MakeFigureLatexReady.m
@@ -1,16 +1,54 @@
-function MakeFigureLatexReady(figureName)
+function MakeFigureLatexReady(figureName,varargin)
 % The purpose of this function is standardize MATLAB figures
 % so they look neat and uniform for latex documents
-
+%
 % Operation: provide a .fig file in the local folder.
 % If it is in a different directory edit the dir value below.
 % The function will produce a .eps file with the same name as
 % the original .fig file in the same directory.  You can also
 % change grid to 'off' to disable grids on figures
+%
+% Options:
+%   - grid 'on' or 'off': enable grid
+%   - dir : String of output directory for figure file
+%   - border [Top,Right;Bottom,Left] : 2x2 matrix of border widths of figure from axes
+%
+% example: MakeFigureLatexReady('myfigure.fig','grid','on')
 
-% example: MakeFigureLatexReady('baseline_occupancy_vs_collisions.fig')
+%%%%%%%%%%%%%%%%%%%
+% Defaults
 grid = 'on';
 dir = '';
+border = [0.09,0.09;0.12,0.12];
+
+%%%%%%%%%%%%%%%%%%%
+% Checks
+if exist(figureName, 'file')~=2
+    error('Figure file does not exist');
+end
+if verLessThan('matlab','8.5')
+    warning('We recommend >=R2015a due to ps/eps file generation issues.');
+end
+% Process PV pairs
+vargs = varargin;
+nargs = length(vargs);
+names = vargs(1:2:nargs);
+values = vargs(2:2:nargs);
+
+validnames = {'grid','border','dir'}; 
+
+for ind = 1:length(names)
+    n = names{ind};
+    v = values{ind};
+    validatestring(n, validnames);
+    switch n
+        case {'grid'}
+            grid = v;
+        case {'border'}
+            border = v;
+    end
+end
+
 
 %%%%%%%%%%%%%%%%%%%
 fig = openfig(figureName);
@@ -36,8 +74,8 @@ fig.Children(index).XGrid = grid;
 fig.Children(index).YGrid = grid;
 % Remove white borders
 %x = fig.CurrentAxes.TightInset.*1; % Default tight widths
-x(3:4) = [0.09,0.09]; % Top and right border widths
-x(1:2) = [0.12,0.12]; % Bottom and left border widths
+x(3:4) = border(1,:); % Top and right border widths
+x(1:2) = border(2,:); % Bottom and left border widths
 fig.CurrentAxes.LooseInset = x;
 fig.PaperPositionMode = 'auto';% Ensure that the size of the saved figure is equal to the size of the figure on the display.
 fig_pos = fig.PaperPosition; 
